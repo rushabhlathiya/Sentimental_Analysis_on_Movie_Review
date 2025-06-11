b593e6eadeb30e301e324346d177982dd77a9838
unknown <rlathiya547@gmail.com>
2025-06-11 23:26:24 +0530

    Added data_prep

---
+++ src/data/data_prep.py
@@ -0,0 +1,167 @@
+import numpy as np
+import pandas as pd
+import os 
+import string
+import re
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from gensim.models import Word2Vec
+from sklearn.model_selection import train_test_split
+import pickle
+
+
+def load_data(filePath:str)->pd.DataFrame:
+    try:
+        return pd.read_csv(filePath)
+    except Exception as e:
+        raise(f"Error Loading Data from {filePath} : {e}")
+# train_data = pd.read_csv("data/raw/train_data.csv")
+# test_data = pd.read_csv("data/raw/test_data.csv")
+
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+stopWords = stopwords.words('english')
+wnl = WordNetLemmatizer()
+translator = str.maketrans("","",string.punctuation)
+
+
+
+
+
+def tokenaization_stopword(text):
+    try:
+        text = text.split()
+        text= [x for x in text if x not in stopWords]
+        text = [wnl.lemmatize(x,pos='v') for x in text]
+        text = " ".join(text)
+        return text
+    except Exception as e:
+        raise Exception(f"Error Lemmatizing Data {e}")
+
+def clean_text(text):
+    try:
+        text = text.lower()
+        text = BeautifulSoup(text, "html.parser").get_text()
+        text = text.translate(translator)
+        text = re.sub(r'\s+'," ",text)
+        return text
+    except Exception as e:
+        raise Exception(f"Error Cleaning Data {e}")
+
+def tfidf_weighted_document_vectors(tokens, doc_tfidf,tfidf_vocab,w2v):
+    try:
+        weighted_vectors = []
+        total_weight = 0
+
+        for word in tokens:
+            if word in w2v.wv and word in tfidf_vocab:
+                # Get TF-IDF weight for this word in current document
+                word_idx = tfidf_vocab[word]
+                weight = doc_tfidf[0, word_idx]
+
+                # Weight the word vector
+                weighted_vectors.append(w2v.wv[word] * weight)
+                total_weight += weight
+
+        if total_weight > 0:
+            return np.sum(weighted_vectors, axis=0) / total_weight
+        else:
+            return np.zeros(w2v.vector_size)
+    except Exception as e:
+        raise Exception(f"Error Tidf Weighted Vectors {e}")
+
+# data_path =os.path.join('data','processed')
+
+# os.makedirs(data_path)
+def save_X_data(df:np.array,filePath:str)->None:
+    try:
+        pd.DataFrame(df).to_csv(filePath,index=False,header=False)
+    except Exception as e:
+        raise Exception(f"Error Saving X :{e}")
+
+def save_y_data(df,filePath:str)->None:
+    try:
+        pd.DataFrame(df).to_csv(filePath,index=False)
+    except Exception as e:
+        raise Exception(f"Error Saving y :{e}")
+        
+# pd.DataFrame(X_train).to_csv(os.path.join(data_path,'X_train.csv'),index=False,header=False)
+# pd.DataFrame(X_test).to_csv(os.path.join(data_path,'X_test.csv'),index=False,header=False)
+# pd.DataFrame(y_train).to_csv(os.path.join(data_path,'y_train.csv'),index=False)
+# pd.DataFrame(y_test).to_csv(os.path.join(data_path,'y_test.csv'),index=False)
+
+def save_object(obj:object,filePath:str)-> None:
+    try:
+        pickle.dump(obj,open(filePath, 'wb'))
+    except Exception as e:
+        raise Exception(f"Error Saving Object : {e}")
+# pickle.dump(tfidf,open('tfidf.pkl', 'wb'))
+# pickle.dump(w2v,open('w2v.pkl', 'wb'))
+
+
+
+
+
+def main():
+    try:
+        raw_filePath = 'data/raw'
+        processed_filePath = os.path.join('data','processed')
+        os.makedirs(processed_filePath)
+        
+        train_data = load_data(os.path.join(raw_filePath,"train_data.csv"))
+        test_data = load_data(os.path.join(raw_filePath,"test_data.csv"))
+        
+        train_data['cleanText'] = train_data['text'].apply(clean_text)
+        test_data['cleanText'] = test_data['text'].apply(clean_text)
+
+
+        train_data['tokenaizeText'] = train_data['cleanText'].apply(tokenaization_stopword)
+        test_data['tokenaizeText'] = test_data['cleanText'].apply(tokenaization_stopword)
+
+
+        tfidf = TfidfVectorizer(ngram_range =(1,2),max_features = 10000)
+        X_train_tfidf=tfidf.fit_transform(train_data['tokenaizeText'])
+        X_test_tfidf=tfidf.transform(test_data['tokenaizeText'])
+
+
+        tfidf_vocab = tfidf.vocabulary_
+        train_data['tokenaizeText'] = [text.split() for text in train_data['tokenaizeText']]
+        test_data['tokenaizeText'] = [text.split() for text in test_data['tokenaizeText']]
+
+        w2v = Word2Vec(
+            sentences=train_data['tokenaizeText'],
+            vector_size=100,  # Embedding dimension
+            window=5,        # Context window size
+            min_count=1,     # Ignore rare words (if < min_count)
+            workers=4        # Parallel processing
+        )
+        X_train = np.array([
+            tfidf_weighted_document_vectors(tokens, X_train_tfidf[i],tfidf_vocab,w2v)
+            for i, tokens in enumerate(train_data['tokenaizeText'])
+        ])
+        X_test = np.array([
+            tfidf_weighted_document_vectors(tokens, X_test_tfidf[i],tfidf_vocab,w2v)
+            for i, tokens in enumerate(test_data['tokenaizeText'])
+        ])
+
+        y_train = train_data['label']
+        y_test = test_data['label']
+        
+        save_X_data(X_train,os.path.join(processed_filePath,'X_train.csv'))
+        save_X_data(X_test,os.path.join(processed_filePath,'X_test.csv'))
+        save_y_data(y_train,os.path.join(processed_filePath,'y_train.csv'))
+        save_y_data(y_test,os.path.join(processed_filePath,'y_test.csv'))
+        
+        save_object(tfidf,'tfidf.pkl')
+        save_object(w2v,'w2v.pkl')
+    except Exception as e:
+        raise Exception(f"An Error Occured {e}")
+    
+
+if __name__=='__main__':
+    main()
